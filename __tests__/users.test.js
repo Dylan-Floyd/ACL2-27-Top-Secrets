eab38c292d389da06bb5e2dbed321fa3f9387ff2
Dylan Floyd <floyd800@gmail.com>
2022-02-08 12:13:45 -0800

    add user sign in test

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -2,13 +2,14 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService.js');
 
 const mockUser = {
   email: 'test@example.com',
   password: '12345',
 };
 
-describe('backend routes', () => {
+describe('users routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
@@ -24,5 +25,13 @@ describe('backend routes', () => {
       id: expect.any(String),
       email: mockUser.email,
     });
+  });
+
+  it('it should sign in a user and allow access to profile', async () => {
+    const agent = request.agent(app);
+    const user = await UserService.create(mockUser);
+    await agent.post('/api/v1/users/sessions').send(mockUser);
+    const res = await agent.get('/api/v1/users/me');
+    expect(res.body.email).toEqual(user.email);
   });
 });
